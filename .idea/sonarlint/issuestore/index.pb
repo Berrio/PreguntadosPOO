7afeee1b3368592926aaadea4e6521bab4be3643
jmedinr <juandiego619@gmail.com>
2022-04-22 00:20:29 -0500

    REFACTOR --- Se agrega preguntas y se refactoriza el código

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+B
+PreguntadosPOO.iml,b\3\b3452de0482d59b79dc09dc1a77eefe7ed05b8d7
--- src/main/java/Pregunta.java
+++ src/main/java/Pregunta.java
@@ -1,47 +1,73 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.List;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 
 public class Pregunta {
 
-    private String pregunta;
-    private ArrayList<Respuesta>ArrayRespuestas;
-    private String categoria;
-    private int dificultad;
+    protected String pregunta;
+    protected String categoria;
+    protected String opcion1;
+    protected String opcion2;
+    protected String opcion3;
+    protected String opcion4;
+    protected String opcionCorrecta;
 
-    public Pregunta(){
-        ArrayRespuestas= new ArrayList<>();
-    }
-    public String getPregunta() {
-        return pregunta;
+    protected List<Pregunta> listPreguntas = new ArrayList<>();
+
+    public Pregunta() throws IOException {
+        leerPreguntas();
     }
 
-    public void setPregunta(String pregunta) {
+    public Pregunta(String pregunta, String categoria, String opcion1, String opcion2, String opcion3,
+                    String opcion4, String opcionCorrecta) {
         this.pregunta = pregunta;
+        this.categoria = categoria;
+        this.opcion1 = opcion1;
+        this.opcion2 = opcion2;
+        this.opcion3 = opcion3;
+        this.opcion4 = opcion4;
+        this.opcionCorrecta = opcionCorrecta;
     }
 
-    public void setPregunta(String pregunta, String categoria, int dificultad) {
-        this.pregunta = pregunta;
-        this.categoria=categoria;
-        this.dificultad=dificultad;
+    //Leer preguntas desde el Json
+    public void leerPreguntas() throws IOException {
+
+        JSONParser jsonParser = new JSONParser();
+        final String dir = System.getProperty("user.dir");
+        try (FileReader reader = new FileReader(dir + "\\PreguntadosPOO\\src\\main\\java\\preguntas.json")) {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+
+            JSONArray preguntasList = (JSONArray) obj;
+
+            //Iterate over song array
+            preguntasList.forEach(emp -> parsePreguntas((JSONObject) emp));
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (org.json.simple.parser.ParseException e) {
+            e.printStackTrace();
+        }
     }
 
-    public String getCategoria() {
-        return categoria;
+    //Agrega las preguntas del Json a un Array
+    public void parsePreguntas(JSONObject preguntas) {
+        JSONObject preguntaObject = (JSONObject) preguntas.get("pregunta");
+
+        this.pregunta = (String) preguntaObject.get("pregunta");
+        this.categoria = (String) preguntaObject.get("categoria");
+        this.opcion1 = (String) preguntaObject.get("opcion1");
+        this.opcion2 = (String) preguntaObject.get("opcion2");
+        this.opcion3 = (String) preguntaObject.get("opcion3");
+        this.opcion4 = (String) preguntaObject.get("opcionCorrecta");
+        this.opcionCorrecta = (String) preguntaObject.get("opcionCorrecta");
+
+        listPreguntas.add(new Pregunta(pregunta,categoria,opcion1,opcion2,opcion3,opcion4,opcionCorrecta));
     }
-
-    public int getDificultad() {
-        return dificultad;
-    }
-
-
-    public void agregarRespuestas(String posicion, String res, boolean escorrecta){
-            Respuesta respuesta=new Respuesta();
-            respuesta.setRespuesta(posicion,res,escorrecta);
-            ArrayRespuestas.add(respuesta);
-    }
-
-    public ArrayList<Respuesta> getArrayRespuestas() {
-        return ArrayRespuestas;
-    }
-
 }
